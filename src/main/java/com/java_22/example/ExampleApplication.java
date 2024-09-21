a0b20b8b3de146773e3e35bfd1b665a244721018
sayed <sayed.ahmed@nextory.com>
2024-09-21 18:57:53 +0530

    docker with redis, mongo, confluent

--- src/main/java/com/java_22/example/ExampleApplication.java
+++ src/main/java/com/java_22/example/ExampleApplication.java
@@ -1,9 +1,14 @@
 package com.java_22.example;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -15,7 +20,7 @@ public class ExampleApplication {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
-	/*@PostConstruct
+	@PostConstruct
 	public void init() {
 		var profile = env.getProperty("spring.profiles.active");
 		if ((Objects.isNull(profile) || "dev".equals(profile)) && Boolean.parseBoolean(env.getProperty("spring.docker.compose.enabled"))) {
@@ -25,7 +30,7 @@ public class ExampleApplication {
 
 	private void startDockerCompose() {
 		try {
-			ProcessBuilder pb = new ProcessBuilder("docker-compose", "-f", "docker-compose.dev.yml", "up", "-d");
+			ProcessBuilder pb = new ProcessBuilder("docker-compose", "-f", "docker-compose.yml", "up", "-d");
 			Process process = pb.start();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			String line;
@@ -36,5 +41,5 @@ public class ExampleApplication {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}*/
+	}
 }
